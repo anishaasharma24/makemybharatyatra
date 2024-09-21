0a0658244a9fc41ae02a0b594aee0730e2adcf59
anishaasharma24 <162309573+anishaasharma24@users.noreply.github.com>
2024-09-21 16:39:21 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -8,11 +8,32 @@
 
 
 
-<div class="container mt-5">
+
+
+   
+<section class="bg-light py-5">
+   <div class="container text-center mb-5 mt-5 about-s-box bg-light ">
     <div class="row">
-        <div class="col-md-6">
-        <h2 id="about-title">ABOUT US</h2>
-        <p class="aboutp-head mb-4">Welcome to MAKE MY BHARAT YATRA PVT LTD</p>
+
+
+   <div class="col-md-6" style="position: relative;">
+
+    <div class="animation-frame" style="position: absolute; right:20%; top: -40px; z-index: 1;">
+        <img src="./images/about-shape-img.png" width="120" alt="">
+    </div>
+
+    <img class="about-travel" src="./images/about-img-3.png" style="position: relative; z-index: 2;" class="shadow rounded-2" width="500px" alt="">
+
+    <div class="caption-img" style="position: absolute; left: 0; bottom: -50px; z-index: 3;">
+        <img src="./images/chotu-keyframe-img.png" style="border: 10px solid white; border-radius: 5px;" width="200px" alt="">
+    </div>
+
+   </div>
+
+
+   <div class="col-md-6 py-5" style="line-height: 35px;">
+    <h2 class="aus" >ABOUT US</h2>
+        <p class="aboutp-head ">Welcome to MAKE MY BHARAT YATRA PVT LTD</p>
         <p class="aboutP-Content">
 At Make My Bharat Yatra PVT LTD, we are committed to providing top-notch Domestic & International Airlines Services that exceed the expectations of our clients and passengers. Our dedicated team of professionals works tirelessly to ensure the smooth operation of airport facilities and deliver exceptional customer service at every touchpoint.</p>
 
@@ -20,63 +41,21 @@ At Make My Bharat Yatra PVT LTD, we are committed to providing top-notch Domesti
 "Discover the magic of travel with MakeMyBharatYatra India. Every journey is an opportunity to create unforgettable memories."
 </p>
 
-<p class="about-f">
-Make My Bharat Yatra was established in 2010 and registered in Domestic & International Airlines Services, Travel & Tourism Trust in 2015 NCT (NATIONAL CAPITAL TERRITORY - Government of India) and our company MSME was registered in 2024.</p>
-        </div>
-
-        <div class="col-md-6 ">
-          <!-- Swiper -->
-  <div class="swiper mySwiper aboutslider">
-    <div class="swiper-wrapper">
-
-      <div class="swiper-slide">
-      <img src="./images/A-1.jpg" alt="">
-      </div>
-
-      <div class="swiper-slide">
-        <img src="./images/A-2.jpg" alt="">
-      </div>
-
-
-      <div class="swiper-slide">
-        <img src="./images/A-3.jpg" alt="">
-      </div>
-
-      <div class="swiper-slide">
-        <img src="./images/A-4.jpg" alt="">
-      </div>
-
-      <div class="swiper-slide">
-        <img src="./images/A-5.jpg" alt="">
-      </div>
-
-      <div class="swiper-slide">
-        <img src="./images/A-6.jpg" alt="">
-      </div>
-
-      <div class="swiper-slide">
-        <img src="./images/A-7.jpg" alt="">
-      </div>
+<p class="aboutP-Content">
+Make My Bharat Yatra was established in 2010 and registered in Domestic & International Airlines Services, Travel & Tourism Trust in 2015 NCT    <b>(NATIONAL CAPITAL TERRITORY - Government of India)</b> and our company MSME was registered in 2024.</p>
+    
+     </div>
 
     </div>
-    <!-- <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div> -->
-  </div>
-
- <div class="Abig-img">
-    <img src="./images/about-second-images.avif" width="100%" alt="">
- </div>
-
-        </div>
-
- 
-</div>
-
-</div>
+   </div>
+</section>
 
 
 
-<div class="container mt-5">
+
+
+
+<div class="container mt-5 ">
     <div class="row ">
         <div class="col-md-10 m-auto shadow">
             <div class="row">
@@ -94,8 +73,8 @@ Make My Bharat Yatra was established in 2010 and registered in Domestic & Intern
        
 </div>
 
-<div class="row mt-5">
-    <div class="col-md-10 m-auto shadow">
+<div class="row">
+    <div class="col-md-10 m-auto shadow mt-5">
         <div class="row">
             <div class="col-md-6 py-5">
                 <h3 class="top-4c">Mission</h3>
@@ -115,39 +94,7 @@ Make My Bharat Yatra was established in 2010 and registered in Domestic & Intern
 
 
 
-    <!-- <div class="row"> -->
-        <!-- <div class="col-md-10 m-auto shadow">
-            <div class="row mt-5">
-               
-             
-                     <div class="col-md-6 px-3">
-                         <h3 class="top-4c">Mission</h3>
-                         <p class="top-4p ">At BharatYatra, our vision is to be the premier provider of unforgettable family vacations, offering unparalleled access to diverse destinations and delivering exceptional service that exceeds expectations.</p>
-                     </div>
 
-                     <div class="col-md-6">
-                        <img src="./images/mission2.jpg"  width="100%" height="100%" alt="">
-                         </div>
-            </div>
-
-        </div>
-       
-
-
-
-        
-    
-            </div>
-           
-    
-
-            
-
-           
-                    </div>
-        
-                </div> -->
-               
         
         
 
@@ -156,7 +103,21 @@ Make My Bharat Yatra was established in 2010 and registered in Domestic & Intern
 
 
 
-    <!-- </div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+</body>
+</html>
 
 
 
