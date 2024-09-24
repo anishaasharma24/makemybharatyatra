2ee141d6530dac3bf989da51f10d745c7e8c71a0
anishaasharma24 <162309573+anishaasharma24@users.noreply.github.com>
2024-09-24 11:33:38 +0530

    Add files via upload

---
+++ career-applynowform.php
@@ -0,0 +1,139 @@
+
+
+<?php include('includes/header.php'); ?>
+
+
+ 
+       
+  
+<div class="container-fluid  form-section ">
+    <h1>Career Appeal Form </h1>
+    <div class="apply-form ">
+        <h2>Apply Now</h2>
+            
+
+            <div class="row form-content">
+                <div class="col-md-6 mb-3">
+                    <label for="inputName" class="form-label">Name</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="First name">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="inputEmail4" class="form-label">Number</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="Last name">
+                </div>
+              </div>
+
+
+              <div class="row form-content">
+                <div class="col-md-6 mb-3">
+                    <label for="inputName" class="form-label">Email</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="First name">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="inputEmail4" class="form-label">Adhar Number</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="Last name">
+                </div>
+            </div>
+
+
+
+              <div class="row form-content">
+                <div class="col-md-6 mb-3">
+                    <label for="inputName" class="form-label">Date Of Birth</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="First name">
+                </div>
+                <div class="col-md-6 mb-4">
+                    <label for="inputEmail4" class="form-label">Pincode</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="Last name">
+                </div>
+              </div>
+
+           
+              <div class="mb-4">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Category</option>
+                <option value="1">General</option>
+                <option value="2">ST/SC</option>
+                <option value="3">OBC</option>
+              </select>
+            </div>
+
+            <div class="mb-4">
+
+              <select class="form-select mb-4" aria-label="Default select example">
+                <option selected>Country</option>
+                <option value="1">India</option>
+              </select>
+
+            </div>
+
+
+
+            <div class="mb-4">
+
+              <select class="form-select" aria-label="Default select example">
+                <option selected>State</option>
+                <option value="1">Andhra Pradesh</option>
+                <option value="2">Arunachal Pradesh</option>
+                <option value="1">Assam</option>
+                <option value="1">Bihar</option>
+                <option value="1">Chhattisgarh</option>
+                <option value="1">Goa</option>
+                <option value="1">Gujarat</option>
+                <option value="1">Haryana</option>
+                <option value="1">Himachal Pradesh</option>
+                <option value="1">Jammu and Kashmir</option>
+                <option value="1">Jharkhand</option>
+                <option value="1">Karnataka</option>
+                <option value="1">Kerala</option>
+                <option value="1">Madhya Pradesh</option>
+                <option value="1">Maharashtra</option>
+                <option value="1">Manipur</option>
+                <option value="1">Meghalaya</option>
+                <option value="1">Mizoram</option>
+                <option value="1">Nagaland</option>
+                <option value="1">Odisha</option>
+                <option value="1">Rajasthan</option>
+                <option value="1">Sikkim</option>
+                <option value="1">Tamil Nadu</option>
+                <option value="1">Telangana</option>
+                <option value="1">Tripura</option>
+                <option value="1">Uttarakhand</option>
+                <option value="1">Uttar Pradesh</option>
+                <option value="1">West Bengal</option>
+                <option value="1">Andaman and Nicobar Islands</option>
+                <option value="1">Chandigarh</option>
+                <option value="1">Dadra and Nagar Haveli</option>
+                <option value="1">Daman and Diu</option>
+                <option value="1">Delhi</option>
+                <option value="1">Lakshadweep</option>
+                <option value="1">Puducherry</option>
+              </select>
+            </div>
+
+
+             <div class="row form-content">
+              <div class="mb-4">
+                <label for="formFile" class="form-label">Photo</label>
+                <input class="form-control" type="file" id="formFile">
+              </div>
+            </div>
+
+
+            <div class="row form-content">
+                <div class="mb-4">
+                  <label for="formFile" class="form-label">Any Diploma Certificate Related To Job Post</label>
+                  <input class="form-control" type="file" id="formFile">
+                </div>
+              </div>
+              
+              
+
+            <button type="submit" class="btn btn-primary mb-5 mt-4" style="font-size: 15px; font-weight: 700; padding: 8px 0px;">Submit Application</button>
+        </form>
+    </div>
+</div>
+
+
+
+<?php include('includes/footer.php'); ?>
